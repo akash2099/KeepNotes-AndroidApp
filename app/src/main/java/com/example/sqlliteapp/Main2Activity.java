8172a4834be85357d510ed53276cff3c67754e61
Akash Manna <akashmanna2099@gmail.com>
2020-08-26 02:01:00 +0530

    Update Main2Activity.java

--- app/src/main/java/com/example/sqlliteapp/Main2Activity.java
+++ app/src/main/java/com/example/sqlliteapp/Main2Activity.java
@@ -2,13 +2,15 @@ package com.example.sqlliteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import java.net.URL;
+
 
 public class Main2Activity extends AppCompatActivity {
     WebView webView;
@@ -20,6 +22,7 @@ public class Main2Activity extends AppCompatActivity {
         webView=(WebView)findViewById(R.id.wv1);
         url="https://github.com/akash2099/Keep-Notes-App";
         webView.setWebViewClient(new mywbview());
+//        webView.canGoBackOrForward(10);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         webView.loadUrl("https://github.com/akash2099/Keep-Notes-App");
@@ -28,8 +31,18 @@ public class Main2Activity extends AppCompatActivity {
     private class mywbview extends WebViewClient{
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
-            view.loadUrl(url);
-            return true;
+            if ("https://github.com/akash2099/Keep-Notes-App".equals(Uri.parse(url).getHost())) {
+                view.loadUrl(url);
+                // This is my website, so do not override; let my WebView load the page
+                return true;
+            }
+            else {
+                // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+
 
         }
 
